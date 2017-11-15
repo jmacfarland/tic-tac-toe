44585b7c0ae66ecec50c5b6a72d3b986901eccc7
Jamison MacFarland <macfarland.jamison@gmail.com>
2017-11-15 09:24:50 -0500

    First commit

---
+++ tic-tac-toe/GameBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tictactoe
+{
+	public class GameBoard
+	{
+		int[,] board;
+
+		public GameBoard ()
+		{
+			board = new int[3,3];
+
+			for (int y = 0; y < 3; y++) 
+			{
+				for (int x = 0; x < 3; x++) 
+				{
+					board [x, y] = 0;
+				}
+			}
+		}
+
+		public void Draw()
+		{
+			for (int y = 0; y < 3; y++) 
+			{
+				for (int x = 0; x < 3; x++) 
+				{
+					Console.Write (board [x, y] + " ");
+				}
+				Console.Write ("\n");
+			}
+		}
+	}
+}
+
---
+++ tic-tac-toe/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tictactoe
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("This is an attempt at playing tic-tac-toe.");
+			Console.WriteLine ("Enjoy!");
+
+			var board = new GameBoard ();
+			board.Draw ();
+		}
+	}
+}
